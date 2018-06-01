0a8bbd4dc33c76dc4478f7163fa2c61b711d20df
TerrierBrakk <32682025+TerrierBrakk@users.noreply.github.com>
2018-06-01 14:58:33 -0500

    Movimiento de plataforma

--- Assets/MovErtical.cs
+++ Assets/MovErtical.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class MovErtical : MonoBehaviour {
 
+	public GameObject platform;
+	public Transform pos_inicial;
+	public Transform pos_final;
+	private Transform pos_sig;
+	public float vel;
+
 	// Use this for initialization
 	void Start () {
-		
+		pos_sig = pos_final;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+		platform.transform.position = Vector2.MoveTowards (platform.transform.position, pos_sig.position, Time.deltaTime * vel);
+
+		if(platform.transform.position ==pos_sig.position)
+		{
+			pos_sig = pos_sig == pos_final ? pos_inicial : pos_final;
+		} 
+	}  
 }
