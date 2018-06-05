b957534f8da28c6bcf6308b8309e1621823d4966
TerrierBrakk <32682025+TerrierBrakk@users.noreply.github.com>
2018-06-04 19:04:23 -0500

    controlscript

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float velocidad = 10;
+	public float VelocidadSalto=10;
+	public float saltobajo=2f;
+	public float saltocaida=2.5f; 
+
+	Transform mytrasn;
+	Rigidbody2D rb;
+
+	bool isGround=false;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D>();
+		mytrasn = this.transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Move (Input.GetAxis ("Horizontal"));
+		Jump ();
+	}
+
+	public void Move(float controlHorizontal)
+	{
+
+	}
+
+	public void Jump()
+	{
+		if (rb.velocity.y < 0) {
+			rb.velocity += Vector2.up * Physics2D.gravity.y * (saltocaida - 1) * Time.deltaTime;
+		} else if (rb.velocity.y > 0 && !Input.GetButton("Jump")){
+			rb.velocity += Vector2.up * Physics2D.gravity.y * (saltobajo - 1) * Time.deltaTime;		}
+		}
+  }
+
