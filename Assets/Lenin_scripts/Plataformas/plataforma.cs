4c22e7524cc0bfd724b5187778b736ff77d40c89
Lenin Figueroa <lenin.figa@gmail.com>
2018-06-28 01:23:48 -0500

    movimiento personahje
    se agrego el script del movimiento del personaje y la camara

---
+++ Assets/Lenin_scripts/Plataformas/plataforma.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataforma : MonoBehaviour {
+    public BoxCollider colisionador;
+    public GameObject Jugador;
+
+	// Use this for initialization
+	void Start () {
+        colisionador.isTrigger = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Jugador.transform.position.y > (transform.position.y + .5f ))
+        {
+            colisionador.isTrigger = false;
+        }
+        else
+        {
+            colisionador.isTrigger = true;
+        }
+        
+    }
+}
