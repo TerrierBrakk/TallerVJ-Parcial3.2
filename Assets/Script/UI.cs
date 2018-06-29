46ca378de1058c175663c160054fd430dfbcb5a9
TerrierBrakk <32682025+TerrierBrakk@users.noreply.github.com>
2018-06-29 01:11:51 -0500

    Avance largo
    UI, Pausa, SceneM

---
+++ Assets/Script/UI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour {
+	private int vidas=3;
+	public Text UIvidas;
+	int daño=1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		UIvidas.text = vidas.ToString ();
+		perdivida ();
+
+	}
+
+
+	void OnCollisionEnter(Collision other)
+	{
+		print ("Has Perdido una vida");
+		if (other.gameObject.CompareTag ("Finish")) 
+		{
+			vidas -= daño;
+			print ("menos una vida");
+		}
+	}
+
+	void perdivida()
+	{
+		if (vidas <= 0) 
+		{
+			death ();
+		}
+	}
+	void death()
+	{		
+			SceneManager.LoadScene ("GameOver");
+	}
+}
